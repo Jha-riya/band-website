0dfc11be1002b2d9a45983f27cd20076d27f59a7
scarlet_witch <riya.jha@ilmtec.in>
2026-01-02 19:20:29 +0530

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AboutPage} from './about-page/about-page';
+import {HomePage} from './home-page/home-page';
+import {StorePage} from './store-page/store-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomePage},
+  {path: 'about', component: AboutPage},
+  {path: 'store', component: StorePage}
+];
