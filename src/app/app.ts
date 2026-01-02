f596001d58b42b9670ae19145d877e5ed283c898
scarlet_witch <riya.jha@ilmtec.in>
2026-01-02 19:20:50 +0530

    imported home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomePage} from './home-page/home-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePage],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
 export class App {
